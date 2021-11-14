16241daf291ed880c4653bc9918ecb6c3bc49710
SHEETAL-MOHANTY <87972424+SHEETAL-MOHANTY@users.noreply.github.com>
2021-11-14 22:47:06 +0530

    ss
    ss

--- static/scripts/responses.js
+++ static/scripts/responses.js
@@ -15,6 +15,44 @@ function getBotResponse(input) {
         return "Talk to you later!";
     } else if(input == "i am bored") {
         return "well fuck u";
+    } else if(input == "What things are you looking forward to?") {
+        return "Hope you can get on it";
+    } else if(input == "Is there anything you want to talk about?") {
+        return "I am hear listening to you let's continue";
+    } else if(input == "When was the last time you were very happy?") {
+        return "See I can feel a smile on your face";
+    } else if(input == "What makes you feel calm?") {
+        return "Great!!";
+    } else if(input == "Where is the place you feel safe?") {
+        return "Hope the thought of that is making you feel safe";
+    } else if(input == "What can I do to help you?") {
+        return "Will be on it but you know I am a bot";
+    } else if(input == "What difficulties are you facing now?") {
+        return "Hope you can get on it";
+    } else if(input == "What things would to like to do in future?") {
+        return "Bravo!!Hope you have positive ideas";
+    } else if(input == "What Are you worried about when you lie in bed and can't sleep?") {
+        return "Always listen to soothing music it might help you can use me for that too";
+    } else if(input == "Can you think of anything fun that we can do at home?") {
+        return "Great Hope on it now and alter share your experience";
+    } else if(input == " How do you feel about things changing?") {
+        return "I am trying to feel it";
+    } else if(input == "Do you have any worries about coronavirus?") {
+        return "Go corona Go !!";
+    } else if(input == "Would you like to bitch about somebody I will ensure your secret is safe here?") {
+        return "huehuehue";
+    } else if(input == "Since you have successfully completed a day treat your sell with a ice cream.") {
+        return "Treat time it is pal";
+    } else if(input == "Remember one person who inspired you the most tell me about them.") {
+        return "Inspiring";
+    } else if(input == "What was your favourite place that you want to visit and why?") {
+        return "I assume it to be beautiful for you";
+    } else if(input == "Please suggest me your favourite street food that you would like me to try.") {
+        return "Yummy Yummy!!";
+    } else if(input == "One secret you would like to share with me?") {
+        return "Its safe here";
+    } else if(input == "Which celeb is your crush?") {
+        return "Awww cutie!!";
     } else {
         return "Try asking something else!";
     }
