22835da43b8d31dff16d53735a948efa49f49c00
sandeep sahoo <sandeepsahoo542@gmail.com>
2021-11-14 23:02:51 +0530

    Update chat.js

--- static/scripts/chat.js
+++ static/scripts/chat.js
@@ -35,7 +35,7 @@ function getTime() {
 
 // Gets the first message
 function firstBotMessage() {
-    let firstMessage = "How's it going buddy and you can think of me a your friend so feel free to share anything"
+    let firstMessage = "How's it going buddy,you can think of me a your friend so feel free to share anything"
     document.getElementById("botStarterMessage").innerHTML = '<p class="botText"><span>' + firstMessage + '</span></p>';
 
     let time = getTime();
